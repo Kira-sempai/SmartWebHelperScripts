05b34090169a77249dd08c23f7e9bf7e22864271
Andrey Vinogradov <andrey.vinogradov@teplomonitor.ru>
2017-09-09 16:36:07 +0300

    scripts: add scripts with sd-card files for projects
    put there sources and destination files paths to copy them to archive and server

---
+++ scripts/sd_card_files_DataLogger.py
@@ -0,0 +1,21 @@
+import func, os
+
+def append(project):
+	buildProjectPath = func.getDeviceBuildDir(project)
+	SDCardFirmwareFileName = func.generateSDCardFirmwareFileName(project)
+
+	project['src_files_list'] = [
+		os.path.join(project['project_path'], 'web/teplomonitor-server/server'),
+		os.path.join(project['project_path'], 'web/teplomonitor-server/sitemenu.txt'),
+		os.path.join(buildProjectPath, 'shared/platform/stm32/langs.sd'),
+		os.path.join(buildProjectPath, 'shared/platform/stm32/dlparams.sd'),
+		os.path.join(buildProjectPath, 'shared/platform/stm32/', SDCardFirmwareFileName),
+	]
+
+	project['dest_files_list'] = [
+		'WEB/',
+		'sitemenu.txt',
+		'langs.sd',
+		'dlparams.sd',
+		'update/firmware.bin',
+	]
---
+++ scripts/sd_card_files_SmartWeb_X2.py
@@ -0,0 +1,22 @@
+import func, os
+
+def append(project):
+	buildProjectPath = func.getDeviceBuildDir(project)
+	SDCardFirmwareFileName = func.generateSDCardFirmwareFileName(project)
+
+	project['src_files_list'] = [
+		os.path.join(project['project_path'], 'web/teplomonitor-server/server'),
+		os.path.join(project['project_path'], 'web/teplomonitor-server/sitemenu.txt'),
+		os.path.join(project['project_path'], 'sdcard/Disco/GUI'),
+		os.path.join(buildProjectPath, 'shared/platform/stm32/dlparams.sd'),
+		os.path.join(buildProjectPath, 'shared/platform/stm32', SDCardFirmwareFileName),
+	]
+	
+	project['dest_files_list'] = [
+		'WEB/',
+		'sitemenu.txt',
+		'GUI/',
+		'dlparams.sd',
+		'update/firmware.bin',
+	]
+	
