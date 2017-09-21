ed5f0923a41bd907c85316fe847104715ada24b7
Andrey Vinogradov <andrey.vinogradov@teplomonitor.ru>
2017-09-21 16:04:50 +0300

    add new projects to list
    need to make them push to server, too

--- build_pack_push_and_clear_all_projects.py
+++ build_pack_push_and_clear_all_projects.py
@@ -20,6 +20,7 @@ def getProjectDestPathPostfix(project):
 	return ' production' if project['production'] == '1' else ' debug'
 
 default_project_path = 'C:/development/xhcc/'
+default_project_path2 = 'C:/development/stdc_clean/'
 
 #=====================================#
 
@@ -27,6 +28,11 @@ DataLogger     = dict(project_path = default_project_path, project = 'device', p
 SmartWeb_Disco = dict(project_path = default_project_path, project = 'device', projectName = 'disco'     , workingName = 'SmartWeb Disco', platform = 'device', production = '1', deviceName = 'DISCO'   , board = '32F746GDISCOVERY', boardVariant = '1', langkey = 'west',)
 SmartWeb_X     = dict(project_path = default_project_path, project = 'device', projectName = 'xhcc'      , workingName = 'SmartWeb X'    , platform = 'device', production = '1', deviceName = 'XHCC'    , board = 'S61'             , boardVariant = '2', langkey = 'west',)
 SmartWeb_X2    = dict(project_path = default_project_path, project = 'device', projectName = 'xhcc_s62'  , workingName = 'SmartWeb X2'   , platform = 'device', production = '1', deviceName = 'XHCC-S62', board = 'S62'             , boardVariant = '2', langkey = 'west',)
+
+SmartWeb_S  = dict(project_path = default_project_path2, project = 'device', projectName = 'stdc'    , workingName = 'SmartWeb S' , platform = 'device', production = '1', deviceName = 'STDC'    , board = 'S20', boardVariant = '3', langkey = 'west',)
+SmartWeb_L  = dict(project_path = default_project_path2, project = 'device', projectName = 'ltdc'    , workingName = 'SmartWeb L' , platform = 'device', production = '1', deviceName = 'LTDC'    , board = 'S40', boardVariant = '3', langkey = 'west',)
+SmartWeb_L2 = dict(project_path = default_project_path2, project = 'device', projectName = 'ltdc_s45', workingName = 'SmartWeb L2', platform = 'device', production = '1', deviceName = 'LTDC_S45', board = 'S45', boardVariant = '3', langkey = 'west',)
+SmartWeb_N  = dict(project_path = default_project_path2, project = 'device', projectName = 'swndin'  , workingName = 'SmartWeb N' , platform = 'device', production = '1', deviceName = 'SWNDIN'  , board = 'S41N', boardVariant = '1', langkey = 'west',)
 
 #=====================================#
 
@@ -44,6 +50,12 @@ projects_array = [
 	SmartWeb_X2,
 ]
 
+projects_array2 = [
+	SmartWeb_S,
+	SmartWeb_L,
+	SmartWeb_L2,
+	SmartWeb_N,
+]
 
 serverDir = "Z:/firmware/"
 
@@ -53,3 +65,5 @@ for project in projects_array:
 	push_project_to_server.do(project, serverDir + project['workingName'] + getProjectDestPathPostfix(project))
 #	clear_project.do(project)
 
+#for project in projects_array2:
+#	build_project.do(project)
