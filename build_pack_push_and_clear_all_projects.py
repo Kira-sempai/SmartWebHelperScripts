8e8c801e944fa7b04a627610e49d0ce94f80418c
Andrey Vinogradov <andrey.vinogradov@teplomonitor.ru>
2017-09-29 11:24:19 +0300

    add SmartWeb S, L, L2, N projects

--- build_pack_push_and_clear_all_projects.py
+++ build_pack_push_and_clear_all_projects.py
@@ -22,19 +22,18 @@ if __name__ == "__main__":
 	default_project_path = 'C:/development/xhcc/'
 	default_project_path2 = 'C:/development/stdc_clean/'
 	
-    
+
 	#=====================================#
 	dataLogger     = Project(default_project_path, 'device', 'DataLogger', 'DataLogger'    , 'device', True, Device('DL'      , 'L30'             , None, True), 'rom')
 	smartWeb_Disco = Project(default_project_path, 'device', 'disco'     , 'SmartWeb Disco', 'device', True, Device('DISCO'   , '32F746GDISCOVERY',    1, True), 'west')
 	smartWeb_X     = Project(default_project_path, 'device', 'xhcc'      , 'SmartWeb X'    , 'device', True, Device('XHCC'    , 'S61'             ,    2, True), 'west')
 	smartWeb_X2    = Project(default_project_path, 'device', 'xhcc_s62'  , 'SmartWeb X2'   , 'device', True, Device('XHCC-S62', 'S62'             ,    2, True), 'west')
+
+	smartWeb_S    = Project(default_project_path2, 'device', 'stdc'    , 'SmartWeb S' , 'device', True, Device('STDC'    , 'S20' , 3, False), 'west')
+	smartWeb_L    = Project(default_project_path2, 'device', 'ltdc'    , 'SmartWeb L' , 'device', True, Device('LTDC'    , 'S40' , 3, False), 'west')
+	smartWeb_L2   = Project(default_project_path2, 'device', 'ltdc_s45', 'SmartWeb L2', 'device', True, Device('LTDC_S45', 'S45' , 1, False), 'west')
+	smartWeb_N    = Project(default_project_path2, 'device', 'swndin'  , 'SmartWeb N' , 'device', True, Device('SWNDIN'  , 'S41N', 1, False), 'west')
 	
-#    SmartWeb_S  = dict(project_path = default_project_path2, project = 'device', projectName = 'stdc'    , workingName = 'SmartWeb S' , platform = 'device', production = '1', deviceName = 'STDC'    , board = 'S20', boardVariant = '3', langkey = 'west',)
-#   SmartWeb_L  = dict(project_path = default_project_path2, project = 'device', projectName = 'ltdc'    , workingName = 'SmartWeb L' , platform = 'device', production = '1', deviceName = 'LTDC'    , board = 'S40', boardVariant = '3', langkey = 'west',)
-#	SmartWeb_L2 = dict(project_path = default_project_path2, project = 'device', projectName = 'ltdc_s45', workingName = 'SmartWeb L2', platform = 'device', production = '1', deviceName = 'LTDC_S45', board = 'S45', boardVariant = '3', langkey = 'west',)
-#	SmartWeb_N  = dict(project_path = default_project_path2, project = 'device', projectName = 'swndin'  , workingName = 'SmartWeb N' , platform = 'device', production = '1', deviceName = 'SWNDIN'  , board = 'S41N', boardVariant = '1', langkey = 'west',)
-	
-    
 	#=====================================#
 	
 	srcPath                = dataLogger.path
@@ -44,11 +43,11 @@ if __name__ == "__main__":
 	sdCardData = [
 		SrcDestData(os.path.join(srcPath  , 'web/teplomonitor-server/server')                , 'WEB/'),
 		SrcDestData(os.path.join(srcPath  , 'web/teplomonitor-server/sitemenu.txt')          , 'sitemenu.txt'),
-        SrcDestData(os.path.join(buildPath, 'shared/platform/stm32/langs.sd')                , 'langs.sd'),
+		SrcDestData(os.path.join(buildPath, 'shared/platform/stm32/langs.sd')                , 'langs.sd'),
 		SrcDestData(os.path.join(buildPath, 'shared/platform/stm32/dlparams.sd')             , 'dlparams.sd'),
 		SrcDestData(os.path.join(buildPath, 'shared/platform/stm32/', SDCardFirmwareFileName), 'firmware.bin')
 	]
-		
+	
 	dataLogger.addSDCardData(sdCardData)
 
 	#=====================================#
@@ -95,21 +94,18 @@ if __name__ == "__main__":
 	
 	smartWeb_X2.addSDCardData(sdCardData)
 	
-    #=====================================#
-    
+	#=====================================#
+	
 	projects_array = [
 		dataLogger,
 		smartWeb_Disco,
-    	smartWeb_X,
+		smartWeb_X,
 		smartWeb_X2,
+		smartWeb_S,
+		smartWeb_L,
+		smartWeb_L2,
+		smartWeb_N,
 	]
-	
-#	projects_array2 = [
-#		SmartWeb_S,
-#		SmartWeb_L,
-#		SmartWeb_L2,
-#   	SmartWeb_N,
-#    ]
 	
 	serverDir = "Z:/firmware/"
 	
@@ -119,7 +115,6 @@ if __name__ == "__main__":
 		push_project_to_server.do(projectItem, serverDir + projectItem.name + getProjectDestPathPostfix(projectItem))
 	#	projectItem.clear()
 	
-	#for project in projects_array2:
-	#	build_project.do(project)
-    
-    
+
+	raw_input(colored("Done. Press ENTER to continue\r", 'white', 'on_green'))
+
