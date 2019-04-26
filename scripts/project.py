926e70c953fcde428d240d62deb5d44c297f02ba
Andrey Vinogradov <andrey.vinogradov@teplomonitor.ru>
2019-04-26 16:56:29 +0300

    scripts/project.py: turn back _production suffix to build path

--- scripts/project.py
+++ scripts/project.py
@@ -94,8 +94,8 @@ class Project(object):
             return str(self.device.boardVariant)
     
     def getProjectDirName(self):
-        postfix = ''
-        #postfix = '_production' if self.production else ''
+        #postfix = ''
+        postfix = '_production' if self.production else ''
         return self.name + postfix + '/'
     
     def getDeviceBuildDir(self):
