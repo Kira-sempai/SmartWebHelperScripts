24311b59ffce19410ab1b725c75fd714e66f644f
andrey.vinogradov <andrey.vinogradov@teplomonitor.ru>
2023-04-25 12:55:47 +0300

    projectsList.py: fix cfg files

--- projectsList.py
+++ projectsList.py
@@ -24,9 +24,9 @@ def getAvailableProjectsList():
 		Project('SW2', 'swk_solar', 'SmartWeb K Solar', Device('SWKS', 'SW-N2', 1, None, True, 'stm32', 'stm32f2x.cfg'), 'west', 'old', False, 'OID_HLOGO'),
 		Project('Other', 'swk_unitTest', 'SmartWeb K UnitTest', Device('SWK', 'SW-N2', 1, None, False, 'stm32', 'stm32f2x.cfg'), 'west', 'old', False, 'OID_HLOGO'),
 		
-		Project('SW2', 'swk_at32'     , 'SmartWeb K2'     , Device('SWK2', 'SW-N3', 1, None, True, 'at32', 'at32f4x.cfg'), 'west', 'old', False, 'OID_HLOGO'),
-		Project('SW2', 'swk_at32_eh'  , 'SmartWeb K21'    , Device('SWK21','SW-N3', 1, None, True, 'at32', 'at32f4x.cfg'), 'west', 'old', False, 'OID_HLOGO'),
-		Project('SW2', 'swk_at32_base', 'SmartWeb K2 Base', Device('SWK2', 'SW-N3', 1, None, True, 'at32', 'at32f4x.cfg'), 'west', 'old', False, 'OID_HLOGO'),
+		Project('SW2', 'swk_at32'     , 'SmartWeb K2'     , Device('SWK2', 'SW-N3', 1, None, True, 'at32', 'at32f407xG.cfg'), 'west', 'old', False, 'OID_HLOGO'),
+		Project('SW2', 'swk_at32_eh'  , 'SmartWeb K21'    , Device('SWK21','SW-N3', 1, None, True, 'at32', 'at32f407xG.cfg'), 'west', 'old', False, 'OID_HLOGO'),
+		Project('SW2', 'swk_at32_base', 'SmartWeb K2 Base', Device('SWK2', 'SW-N3', 1, None, True, 'at32', 'at32f407xG.cfg'), 'west', 'old', False, 'OID_HLOGO'),
 		
 		Project('Caleon', 'caleon_clima', 'Caleon'        , Device('caleon_clima', 'RC40',    1, None, False, 'stm32n'), 'rom', 'new'),
 		Project('Other',  'caleon_brv'  , 'Caleon BRV'    , Device('caleon_brv'  , 'RC50', None, None, False, 'stm32n'), 'rom', 'new'),
